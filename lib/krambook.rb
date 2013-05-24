45e9715b5cd4f4fe0ea5a68a4a1911bfb6190a49
Ken Erickson <k@2052.me>
2013-05-23 19:18:18 -0700

    fix indentation so it uses spaces. oops

--- lib/krambook.rb
+++ lib/krambook.rb
@@ -9,27 +9,27 @@ module Krambook
 
     class_option :format, :desc => 'The format to convert to', :aliases => '-f', :default => 'kramdown', :type => :string
     class_option :help, :type => :boolean, :desc => 'Show help usage' 
-		default_task :join
+    default_task :join
 
-	  desc 'join', 'Joins the markdown files'
+    desc 'join', 'Joins the markdown files'
     def join
-    	files            = []
-			joined_file_path = File.expand_path './manuscript/' + 'Joined.md'
-			File.delete(joined_file_path) if File.exist?(joined_file_path)
+      files            = []
+      joined_file_path = File.expand_path './manuscript/' + 'Joined.md'
+      File.delete(joined_file_path) if File.exist?(joined_file_path)
 
-			File.open(File.expand_path('./manuscript/Book.txt'), 'r') do |f|
-				f.each_line do |line|
-					files << line.gsub(/\s+/, "")
-				end
-			end
+      File.open(File.expand_path('./manuscript/Book.txt'), 'r') do |f|
+        f.each_line do |line|
+          files << line.gsub(/\s+/, "")
+        end
+      end
 
-			File.open(joined_file_path, 'a') do |f|
-				files.each do |file_path|
-					full_file_path = File.expand_path './manuscript/' + file_path
-					f.puts Kramdown::Document.new(IO.read(full_file_path)).send("to_#{options[:format]}".to_sym) if File.exists?(full_file_path)
-					f.puts '' unless file_path == files.last
-				end
-			end
+      File.open(joined_file_path, 'a') do |f|
+        files.each do |file_path|
+          full_file_path = File.expand_path './manuscript/' + file_path
+          f.puts Kramdown::Document.new(IO.read(full_file_path)).send("to_#{options[:format]}".to_sym) if File.exists?(full_file_path)
+          f.puts '' unless file_path == files.last
+        end
+      end
     end
   end
 end
--- lib/kramdown/converter/includey.rb
+++ lib/kramdown/converter/includey.rb
@@ -1,16 +1,16 @@
 module Kramdown
-	module Converter
-		class Includey < ::Kramdown::Converter::Kramdown
-			def convert_codeblock(el, indent)
-				if el.attr.include?('include')
-					file_path = File.expand_path(Dir.pwd + '/' + el.attr['include'])
-					el.value  = IO.read(file_path) if File.exists?(file_path)
-				end
+  module Converter
+    class Includey < ::Kramdown::Converter::Kramdown
+      def convert_codeblock(el, indent)
+        if el.attr.include?('include')
+          file_path = File.expand_path(Dir.pwd + '/' + el.attr['include'])
+          el.value  = IO.read(file_path) if File.exists?(file_path)
+        end
 
-				attr = el.attr.dup
-				lang = extract_code_language!(attr)  
-				"~~~ #{lang}\n" + el.value.split(/\n/).map {|l| l.empty? ? "" : "#{l}"}.join("\n") + "\n~~~\n"
-		  end
-		end
-	end
+        attr = el.attr.dup
+        lang = extract_code_language!(attr)  
+        "~~~ #{lang}\n" + el.value.split(/\n/).map {|l| l.empty? ? "" : "#{l}"}.join("\n") + "\n~~~\n"
+      end
+    end
+  end
 end
